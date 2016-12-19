d3f0ba44b088d0e5b885e187347a7fe9ef983d2b
Owen Peredo <owenperedo@gmail.com>
2016-12-19 17:40:24 -0400

    initial commit

--- app/controllers/plugins/cama_image_lightbox/admin_controller.rb
+++
@@ -1,8 +0,0 @@
-class Plugins::CamaImageLightbox::AdminController < CamaleonCms::Apps::PluginsAdminController
-  include Plugins::CamaImageLightbox::MainHelper
-  def index
-    # actions for admin panel
-  end
-
-  # add custom methods below
-end
--- app/controllers/plugins/cama_image_lightbox/front_controller.rb
+++
@@ -1,8 +0,0 @@
-class Plugins::CamaImageLightbox::FrontController < CamaleonCms::Apps::PluginsFrontController
-  include Plugins::CamaImageLightbox::MainHelper
-  def index
-    # actions for frontend module
-  end
-
-  # add custom methods below
-end
